c2a564683fbbec4a85d44acbe3aa9ff3e82c7fa5
xuyunfan9991 <143389390+xuyunfan9991@users.noreply.github.com>
2025-11-07 15:42:56 +0800

    Fix train_single dataset loading

--- train/train_single.py
+++ train/train_single.py
@@ -3,7 +3,14 @@ from model import *
 from torch import randperm
 from torch._utils import _accumulate
 #from utils_torch import progress_bar
-from train_multiple import crossent, train, test
+
+
+def crossent(y_pred, y_true):
+    """Categorical cross entropy for sequence outputs."""
+    return - torch.mean(
+        y_true[:, 0, :] * torch.log(y_pred[:, 0, :] + 1e-10)
+        + y_true[:, 1, :] * torch.log(y_pred[:, 1, :] + 1e-10)
+    )
 
 assert torch.cuda.is_available()
 
@@ -36,6 +43,7 @@ model.load_state_dict(torch.load(sys.argv[2]))
 
 bce = nn.BCELoss()
 
+
 def loss(y_pred, y_true):
     y_pred = torch.split(y_pred, [2,1,2,1,2,1,2,1], dim=1)
     y_true = torch.split(y_true, [2,1,2,1,2,1,2,1], dim=1)
@@ -43,7 +51,10 @@ def loss(y_pred, y_true):
     if TISSUE % 2 == 0:
         loss = crossent(y_pred[TISSUE], y_true[TISSUE])
     else:
-        loss = bce(y_pred[TISSUE][y_true[TISSUE]>=0], y_true[TISSUE][y_true[TISSUE]>=0])
+        loss = bce(
+            y_pred[TISSUE][y_true[TISSUE] >= 0],
+            y_true[TISSUE][y_true[TISSUE] >= 0],
+        )
 
     return loss
 
@@ -55,9 +66,47 @@ scheduler = torch.optim.lr_scheduler.CosineAnnealingWarmRestarts(optimizer, T_0,
 iters = len(train_dl)
 flog_final = open("log.%s.%s.txt" % (model_num, TISSUE), 'w')
 
+
+def train_epoch(epoch):
+    model.train()
+    train_loss = 0.0
+    num_batches = 0
+
+    for batch_idx, (inputs, targets) in enumerate(train_dl, start=1):
+        inputs, targets = inputs.cuda(), targets.cuda()
+        outputs = model(inputs)
+        batch_loss = criterion(outputs, targets)
+        train_loss += float(batch_loss)
+
+        optimizer.zero_grad()
+        batch_loss.backward()
+        optimizer.step()
+        scheduler.step(epoch + (batch_idx - 1) / max(1, iters))
+
+        num_batches = batch_idx
+
+    return train_loss / max(1, num_batches)
+
+
+def evaluate():
+    model.eval()
+    test_loss = 0.0
+    num_batches = 0
+
+    for batch_idx, (inputs, targets) in enumerate(val_dl, start=1):
+        inputs, targets = inputs.cuda(), targets.cuda()
+        with torch.no_grad():
+            outputs = model(inputs)
+            batch_loss = criterion(outputs, targets)
+        test_loss += float(batch_loss)
+        num_batches = batch_idx
+
+    return test_loss / max(1, num_batches)
+
+
 for epoch in range(0, 4):
-    train_loss = train(epoch)
-    test_loss = test()
+    train_loss = train_epoch(epoch)
+    test_loss = evaluate()
     print(epoch, train_loss, test_loss, file=flog_final)
     flog_final.flush()
     torch.save(model.state_dict(), "models/final.%s.%s.%s" % (model_num, TISSUE, epoch))
